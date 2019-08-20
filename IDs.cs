ffe661760766e1b312dd1a7cb7312e4f5239e563
direwolf420 <amakejk1@web.de>
2019-08-20 22:06:38 +0200

    Some fixes to the multiplayer stuff so it might work properly

--- IDs.cs
+++ IDs.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace BossAssist
 {
 	internal enum MessageType : byte
@@ -8,25 +9,26 @@ namespace BossAssist
 		DeathCount
 	}
 
+    [Flags]
 	internal enum RecordID : int
 	{
-		None,
-		Kills,
-		Deaths,
-		ShortestFightTime,
-		LongestFightTime,
-		DodgeTime,
-		MostHits,
-		LeastHits,
-		BestBrink,
-		BestBrinkPercent,
-		WorstBrink,
-		WorstBrinkPercent,
+		None = 0,
+		Kills = 1,
+		Deaths = 2,
+        ShortestFightTime = 4,
+        LongestFightTime = 8,
+        DodgeTime = 16,
+        MostHits = 32,
+        LeastHits = 64,
+        BestBrink = 128,
+        BestBrinkPercent = 256,
+        WorstBrink = 512,
+        WorstBrinkPercent = 1024,
 
-		LastFightTime,
-		LastDodgeTime,
-		LastHits,
-		LastBrink,
-		LastBrinkPercent
+        LastFightTime = 2048,
+        LastDodgeTime = 4096,
+        LastHits = 8192,
+        LastBrink = 16384,
+		LastBrinkPercent = 32768
 	}
 }
